21687cb4e4ffae1732ffc1c666d4826239cfa0ff
Haitham Estetieh <iamhaitham2011@gmail.com>
2021-09-26 10:34:14 +0300

    Building a Search Typeahead - debounceTime and distinctUntilChanged Operators

--- RxJS In Practice Course/src/app/course/course.component.ts
+++ RxJS In Practice Course/src/app/course/course.component.ts
@@ -7,10 +7,10 @@ import {
 } from "@angular/core";
 import { ActivatedRoute } from "@angular/router";
 import { Course } from "../model/course";
-import { Observable } from "rxjs";
+import { fromEvent, Observable } from "rxjs";
 import { Lesson } from "../model/lesson";
 import { createHttpObservable } from "../util";
-import { map } from "rxjs/operators";
+import { debounceTime, distinctUntilChanged, map } from "rxjs/operators";
 
 @Component({
   selector: "course",
@@ -21,19 +21,27 @@ export class CourseComponent implements OnInit, AfterViewInit {
   course$: Observable<Course[]>;
   lessons$: Observable<Lesson[]>;
 
-  @ViewChild("searchInput", { static: true }) input: ElementRef;
+  @ViewChild("searchInput", { static: true }) mySearchInput: ElementRef;
 
   constructor(private route: ActivatedRoute) {}
 
   ngOnInit() {
     const courseId = this.route.params["id"];
 
-    this.course$ = createHttpObservable(`/api/course/${courseId}`);
+    this.course$ = createHttpObservable(`/api/courses/${courseId}`);
     
     this.lessons$ = createHttpObservable(
       `/api/lessons?courseId=${courseId}&pageSize=100`
     ).pipe(map((res) => res["payload"]));
   }
 
-  ngAfterViewInit() {}
+  ngAfterViewInit() {
+
+    fromEvent<any>(this.mySearchInput.nativeElement,'keyup').pipe(
+      map(event=>event.target.value),
+      debounceTime(400),
+      distinctUntilChanged(),
+    ).subscribe(val=>console.log(val));
+
+  }
 }
