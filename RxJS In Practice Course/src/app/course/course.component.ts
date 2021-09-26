6695b462d4d9405b7abb4c64e8105a1c901ab337
Haitham Estetieh <iamhaitham2011@gmail.com>
2021-09-26 10:19:55 +0300

    Setting Up the Course Component

--- RxJS In Practice Course/src/app/course/course.component.ts
+++ RxJS In Practice Course/src/app/course/course.component.ts
@@ -1,57 +1,39 @@
-import {AfterViewInit, Component, ElementRef, OnInit, ViewChild} from '@angular/core';
-import {ActivatedRoute} from "@angular/router";
-import {Course} from "../model/course";
 import {
-    debounceTime,
-    distinctUntilChanged,
-    startWith,
-    tap,
-    delay,
-    map,
-    concatMap,
-    switchMap,
-    withLatestFrom,
-    concatAll, shareReplay
-} from 'rxjs/operators';
-import {merge, fromEvent, Observable, concat} from 'rxjs';
-import {Lesson} from '../model/lesson';
-
+  AfterViewInit,
+  Component,
+  ElementRef,
+  OnInit,
+  ViewChild,
+} from "@angular/core";
+import { ActivatedRoute } from "@angular/router";
+import { Course } from "../model/course";
+import { Observable } from "rxjs";
+import { Lesson } from "../model/lesson";
+import { createHttpObservable } from "../util";
+import { map } from "rxjs/operators";
 
 @Component({
-    selector: 'course',
-    templateUrl: './course.component.html',
-    styleUrls: ['./course.component.css']
+  selector: "course",
+  templateUrl: "./course.component.html",
+  styleUrls: ["./course.component.css"],
 })
 export class CourseComponent implements OnInit, AfterViewInit {
+  course$: Observable<Course[]>;
+  lessons$: Observable<Lesson[]>;
 
+  @ViewChild("searchInput", { static: true }) input: ElementRef;
 
-    course$: Observable<Course>;
-    lessons$: Observable<Lesson[]>;
+  constructor(private route: ActivatedRoute) {}
 
+  ngOnInit() {
+    const courseId = this.route.params["id"];
 
-    @ViewChild('searchInput', { static: true }) input: ElementRef;
+    this.course$ = createHttpObservable(`/api/course/${courseId}`);
+    
+    this.lessons$ = createHttpObservable(
+      `/api/lessons?courseId=${courseId}&pageSize=100`
+    ).pipe(map((res) => res["payload"]));
+  }
 
-    constructor(private route: ActivatedRoute) {
-
-
-    }
-
-    ngOnInit() {
-
-        const courseId = this.route.snapshot.params['id'];
-
-
-
-    }
-
-    ngAfterViewInit() {
-
-
-
-
-    }
-
-
-
-
+  ngAfterViewInit() {}
 }
