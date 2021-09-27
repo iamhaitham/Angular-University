ab010857e5a94844b8e141f1d92b1dccc2d3585d
Haitham Estetieh <iamhaitham2011@gmail.com>
2021-09-27 15:08:52 +0300

    Defining NgRx Actions using Action Creators

--- NgRx (with NgRx Data) - The Complete Guide/src/app/auth/login/login.component.ts
+++ NgRx (with NgRx Data) - The Complete Guide/src/app/auth/login/login.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit, ViewEncapsulation } from "@angular/core";
 import { FormBuilder, FormGroup, Validators } from "@angular/forms";
 
 import { Store } from "@ngrx/store";
-
+import { login } from "../auth.actions";
 import { AuthService } from "../auth.service";
 import { tap } from "rxjs/operators";
 import { noop } from "rxjs";
@@ -38,7 +38,7 @@ export class LoginComponent implements OnInit {
       .pipe(
         tap((user) => {
           console.log(user);
-          this.store.dispatch()
+          this.store.dispatch(login({ user: user }));
           this.router.navigateByUrl("/courses");
         })
       )
